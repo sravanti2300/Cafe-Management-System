85c3560643b696a6c8e3e2fbd5afadc3a62a9a05
sravanti2300 <sravanti.kanchi@gmail.com>
2023-12-21 15:49:44 -0500

    changes

---
+++ CafeManagement/src/main/java/edu/neu/cafemanagement/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.neu.cafemanagement;
+
+/**
+ *
+ * @author sravanti
+ */
+public class Constants {
+
+    // Store database credentials here
+    public final static String dbUrl = "jdbc:mysql://localhost:3306/cafedb";
+    public final static String dbUser = "root";
+    public final static String dbPwd = "Ush@1973";
+
+}
---
+++ CafeManagement/src/main/java/edu/neu/csye6200/ShakeFactoryEagerSingleton.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.neu.csye6200;
+
+/**
+ *
+ * @author sravanti
+ */
+public class ShakeFactoryEagerSingleton implements CafeItemFactoryAPI {
+    private final static ShakeFactoryEagerSingleton instance = new ShakeFactoryEagerSingleton();
+
+    private ShakeFactoryEagerSingleton() {
+    }
+
+    public static ShakeFactoryEagerSingleton getInstance() {
+        return instance;
+    }
+
+    @Override
+    public CafeItem createCafeItemInstance(String name, int price) {
+        return new ShakeItem(name, price);
+    }
+}
